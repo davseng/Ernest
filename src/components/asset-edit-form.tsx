c3e5eb9c1b5188e1a3a9a594b8e005ca3b4db6c1
davseng <davseng@gmail.com>
2026-09-05 00:22:00 -0400

    Add optional registration field to asset editor

--- src/components/asset-edit-form.tsx
+++ src/components/asset-edit-form.tsx
@@ -9,6 +9,7 @@ export function AssetEditForm({ asset, action }: { asset: Asset; action: (data: 
         <label>Make<input name="make" defaultValue={asset.make} maxLength={100} required /></label>
         <label>Model<input name="model" defaultValue={asset.model} maxLength={100} required /></label>
         <label>Year<input name="year" type="number" min="1800" max="3000" step="1" defaultValue={asset.year} required /></label>
+        <label>Registration / VIN <span>(optional)</span><input name="registrationNumber" defaultValue={asset.registrationNumber} maxLength={100} /></label>
       </div>
       <label>Summary<textarea name="summary" defaultValue={asset.summary} rows={5} maxLength={1000} required /></label>
       <button className="primary-button" type="submit">Save changes</button>
